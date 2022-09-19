a3a6c2617f6222a26572f64179f45422a5a22ce3
katori-ouo <fjj927@outlook.com>
2022-09-19 15:59:27 +0800

    添加了程序的config文件

---
+++ audio_config.h
@@ -0,0 +1,11 @@
+#ifndef AUDIO_CONFIG_H_
+#define AUDIO_CONFIG_H_
+
+#define Q15MOD                  32768
+#define Q31MOD                  2147483648
+#define Q30MOD                  1073741824
+#define ANAL_BLOCKL_MAX         256 /* Max analysis block length */
+#define FRAME_LEN               128 /* Max frame length */
+#define HALF_ANAL_BLOCKL        129 /* Half max analysis block length + 1 */
+
+#endif /* AUDIO_CONFIG_H_ */
